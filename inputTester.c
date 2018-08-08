24505ea0aeddadbc7780d8c6ecdf20adb229fbae
Alfieri, Andrea <andrea_alfieri@bmc.com>
2018-08-08 15:11:57 +0200

    testing

--- inputTester.c
+++ inputTester.c
@@ -2,8 +2,30 @@
 #include<stdlib.h>
 
 int main(int argc, char *argv[]){
-  char str = argv[1];
-  printf("%s\n", str);
+  char *str = NULL;
+  FILE *fp = NULL;
 
+  char *temp = NULL;
+  size_t len, nread;
+
+
+  if(argc != 2){
+    fprintf(stderr, "Errore chiamata programma\n");
+    return -1;
+  }
+
+  str = argv[1];
+  fp = fopen(str, "r");
+
+  if(!fp){
+    fprintf(stderr, "Errore apertura file\n");
+    return -1;
+  }
+
+  nread = getline(&temp, &len, fp);
+  printf("%s\n", temp);
+
+
+  fclose(fp);
   return 0;
 }
